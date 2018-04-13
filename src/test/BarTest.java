80f213b49bca13b4a621b8c1897758750299813a
Guilherme Deconto <deconto100@gmail.com>
2018-04-12 23:58:06 -0300

    Alteração nas variaiveis de teste da classe BarTest

--- src/test/BarTest.java
+++ src/test/BarTest.java
@@ -61,105 +61,105 @@ public class BarTest {
 
 	@Test
 	public void testaQuantidadeDeHomens() {
-		Cliente c5 = new Cliente("Lucindo", "01531075029", 20, "M");
-		Cliente c6 = new Cliente("Ademir", "4676900545", 20, "M");
-		Cliente c7 = new Cliente("Julio", "01531075029", 20, "M");
-		Cliente c8 = new Cliente("Alicia", "4676900545", 20, "F");
-		b.entrada(c5);
-		b.entrada(c6);
-		b.entrada(c7);
-		b.entrada(c8);
+		Cliente c1 = new Cliente("Lucindo", "01531075029", 20, "M");
+		Cliente c2 = new Cliente("Ademir", "4676900545", 20, "M");
+		Cliente c3 = new Cliente("Julio", "16786254901", 20, "M");
+		Cliente c4 = new Cliente("Alicia", "15627314614", 20, "F");
+		b.entrada(c1);
+		b.entrada(c2);
+		b.entrada(c3);
+		b.entrada(c4);
 		assertEquals(6, b.publicoMasculino());
 	}
 
 	@Test
 	public void testaQuantidadeDeMulheres() {
-		Cliente c5 = new Socio("Lucinda", "01531075029", 26, "F", 109);
-		Cliente c6 = new Cliente("Maria Lucia", "4676900545", 27, "F");
-		Cliente c7 = new Cliente("Nicole", "01531075029", 22, "F");
-		Cliente c8 = new Cliente("Maria", "4676900545", 19, "F");
-		b.entrada(c5);
-		b.entrada(c6);
-		b.entrada(c7);
-		b.entrada(c8);
+		Cliente c1 = new Socio("Lucinda", "01531075029", 26, "F", 109);
+		Cliente c2 = new Cliente("Maria Lucia", "4676900545", 27, "F");
+		Cliente c3 = new Cliente("Nicole", "16786254901", 22, "F");
+		Cliente c4 = new Cliente("Maria", "15627314614", 19, "F");
+		b.entrada(c1);
+		b.entrada(c2);
+		b.entrada(c3);
+		b.entrada(c4);
 		assertEquals(5, b.publicoFeminino());
 	}
 
 	@Test
 	public void testaPublicSocio() {
-		Cliente c5 = new Socio("Lucinda", "01531075029", 26, "F", 109);
-		Cliente c6 = new Socio("Maria Lucia", "4676900545", 27, "F", 056);
-		Cliente c7 = new Socio("Nicole", "01531075029", 22, "F", 002);
-		Cliente c8 = new Socio("Maria", "4676900545", 19, "F", 334);
-		b.entrada(c5);
-		b.entrada(c6);
-		b.entrada(c7);
-		b.entrada(c8);
+		Cliente c1 = new Socio("Lucinda", "01531075029", 26, "F", 109);
+		Cliente c2 = new Socio("Maria Lucia", "4676900545", 27, "F", 056);
+		Cliente c3 = new Socio("Nicole", "16786254901", 22, "F", 002);
+		Cliente c4 = new Socio("Maria", "15627314614", 19, "F", 334);
+		b.entrada(c1);
+		b.entrada(c2);
+		b.entrada(c3);
+		b.entrada(c4);
 		assertEquals(4, b.publicoSocio());
 	}
 
 	@Test
 	public void testaPublicoNaoSocio() {
-		Cliente c5 = new Socio("Lucinda", "01531075029", 26, "F", 109);
-		Cliente c6 = new Cliente("Maria Lucia", "4676900545", 27, "F");
-		Cliente c7 = new Cliente("Nicole", "01531075029", 22, "F");
-		Cliente c8 = new Cliente("Maria", "4676900545", 19, "F");
-		b.entrada(c5);
-		b.entrada(c6);
-		b.entrada(c7);
-		b.entrada(c8);
+		Cliente c1 = new Socio("Lucinda", "01531075029", 26, "F", 109);
+		Cliente c2 = new Cliente("Maria Lucia", "4676900545", 27, "F");
+		Cliente c3 = new Cliente("Nicole", "16786254901", 22, "F");
+		Cliente c4 = new Cliente("Maria", "15627314614", 19, "F");
+		b.entrada(c1);
+		b.entrada(c2);
+		b.entrada(c3);
+		b.entrada(c4);
 		assertEquals(7, b.publicoNormal());
 	}
 
 	@Test
 	public void testaPercentualDeMulheres() {
-		Cliente c5 = new Socio("Lucinda", "01531075029", 26, "F", 109);
-		Cliente c6 = new Cliente("Maria Lucia", "4676900545", 27, "F");
-		Cliente c7 = new Cliente("Nicole", "01531075029", 22, "F");
-		Cliente c8 = new Cliente("Maria", "4676900545", 19, "F");
-		b.entrada(c5);
-		b.entrada(c6);
-		b.entrada(c7);
-		b.entrada(c8);
+		Cliente c1 = new Socio("Lucinda", "01531075029", 26, "F", 109);
+		Cliente c2 = new Cliente("Maria Lucia", "4676900545", 27, "F");
+		Cliente c3 = new Cliente("Nicole", "16786254901", 22, "F");
+		Cliente c4 = new Cliente("Maria", "15627314614", 19, "F");
+		b.entrada(c1);
+		b.entrada(c2);
+		b.entrada(c3);
+		b.entrada(c4);
 		assertEquals(63, b.percentMulheres());
 	}
 
 	@Test
 	public void testaPercentualDeHomens() {
-		Cliente c5 = new Socio("Lucinda", "01531075029", 26, "F", 109);
-		Cliente c6 = new Cliente("Maria Lucia", "4676900545", 27, "F");
-		Cliente c7 = new Cliente("Nicole", "01531075029", 22, "F");
-		Cliente c8 = new Cliente("Maria", "4676900545", 19, "F");
-		b.entrada(c5);
-		b.entrada(c6);
-		b.entrada(c7);
-		b.entrada(c8);
+		Cliente c1 = new Socio("Lucinda", "01531075029", 26, "F", 109);
+		Cliente c2 = new Cliente("Maria Lucia", "4676900545", 27, "F");
+		Cliente c3 = new Cliente("Nicole", "16786254901", 22, "F");
+		Cliente c4 = new Cliente("Maria", "15627314614", 19, "F");
+		b.entrada(c1);
+		b.entrada(c2);
+		b.entrada(c3);
+		b.entrada(c4);
 		assertEquals(38, b.percentHomens());
 	}
 
 	@Test
 	public void testaPercentualDeSocios() {
-		Cliente c5 = new Socio("Lucinda", "01531075029", 26, "F", 109);
-		Cliente c6 = new Cliente("Maria Lucia", "4676900545", 27, "F");
-		Cliente c7 = new Cliente("Nicole", "01531075029", 22, "F");
-		Cliente c8 = new Cliente("Maria", "4676900545", 19, "F");
-		b.entrada(c5);
-		b.entrada(c6);
-		b.entrada(c7);
-		b.entrada(c8);
+		Cliente c1 = new Socio("Lucinda", "01531075029", 26, "F", 109);
+		Cliente c2 = new Cliente("Maria Lucia", "4676900545", 27, "F");
+		Cliente c3 = new Cliente("Nicole", "16786254901", 22, "F");
+		Cliente c4 = new Cliente("Maria", "15627314614", 19, "F");
+		b.entrada(c1);
+		b.entrada(c2);
+		b.entrada(c3);
+		b.entrada(c4);
 		assertEquals(13, b.percentSocios());
 	}
 
 	@Test
 	public void testaPercentualDeNaoSocios() {
-		Cliente c5 = new Socio("Lucinda", "01531075029", 26, "F", 109);
-		Cliente c6 = new Cliente("Maria Lucia", "4676900545", 27, "F");
-		Cliente c7 = new Cliente("Nicole", "01531075029", 22, "F");
-		Cliente c8 = new Cliente("Maria", "4676900545", 19, "F");
-		b.entrada(c5);
-		b.entrada(c6);
-		b.entrada(c7);
-		b.entrada(c8);
+		Cliente c1 = new Socio("Lucinda", "01531075029", 26, "F", 109);
+		Cliente c2 = new Cliente("Maria Lucia", "4676900545", 27, "F");
+		Cliente c3 = new Cliente("Nicole", "16786254901", 22, "F");
+		Cliente c4 = new Cliente("Maria", "15627314614", 19, "F");
+		b.entrada(c1);
+		b.entrada(c2);
+		b.entrada(c3);
+		b.entrada(c4);
 		assertEquals(88, b.percentNormal());
 	}
 	
