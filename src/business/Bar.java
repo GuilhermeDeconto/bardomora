db0460cfe9909b1f40976d83710418fcd5095a5d
Guilherme Deconto <deconto100@gmail.com>
2018-04-12 21:31:02 -0300

    Criado novos metodos na classe Bar, publico, publicoH, publicoF e verificações

--- src/business/Bar.java
+++ src/business/Bar.java
@@ -49,6 +49,52 @@ public class Bar {
 		}
 		return numHomens;
 	}
+	public int publicoFeminino() {
+		int numMulheres = 0;
+		for (int i = 0; i < clientes.size(); i++) {
+			if (clientes.get(i).getGenero().equalsIgnoreCase("F")) {
+				numMulheres++;
+			}
+		}
+		return numMulheres;
+	}
+
+	public int publicoSocio(){
+		int numSocios = 0;
+		for (int i =0; i< clientes.size(); i++){
+			if(clientes.get(i) instanceof Socio){
+				numSocios++;
+			}
+		}
+		return numSocios;
+	}
+
+	public int publicoNormal(){
+		int numNormal = 0;
+		numNormal = clientes.size() - publicoSocio();
+		return numNormal;
+	}
+
+	public int percentHomens(){
+		int phomens = Math.round(((float) publicoMasculino() / (float) publico()) * 100);
+		return phomens;
+	}
+
+	public int percentMulheres() {
+		int pmulheres = Math.round(((float) publicoFeminino() / (float) publico()) * 100);
+		return pmulheres;
+	}
+
+	public int percentSocios(){
+		int psocios = Math.round(((float) publicoSocio()/ (float)publico())*100);
+		return psocios;
+	}
+
+	public int percentNormal(){
+		int pnormal = Math.round(((float) publicoNormal()/ (float)publico())*100);
+		return pnormal;
+	}
+
 
 	@Override
 	public String toString() {
