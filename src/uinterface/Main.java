d99fc6377e2835e94976b70f7d012240c6de6060
Guilherme Deconto <deconto100@gmail.com>
2018-04-12 21:45:41 -0300

    Modificação na classe main, adicionando cliente e socios e imprimindo a quantidade de clientes, homens e mulheres no estabelecimento

--- src/uinterface/Main.java
+++ src/uinterface/Main.java
@@ -1,8 +1,27 @@
 package uinterface;
 
+import business.Bar;
+import persistence.Cliente;
+import persistence.Socio;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+        Cliente c1 = new Cliente("Vitor Luiz", "98261823713", 22, "M");
+        Cliente c2 = new Cliente("Alicia", "42365375458", 25, "F");
+        Cliente c5 = new Cliente("Yuri","16786254901",30,"M");
+        Cliente c3 = new Socio("Debora", "13245982712", 18, "F", 001);
+        Cliente c4 = new Socio("Guilherme", "72530172531", 18, "M", 000);
+        Bar mora = new Bar();
+
+        mora.entrada(c1);
+        mora.entrada(c2);
+        mora.entrada(c3);
+        mora.entrada(c4);
+        mora.entrada(c5);
+
+        System.out.println("quantidade de clientes: "+ mora.publico());
+        System.out.println("quantidade de clientes homens: "+ mora.publicoMasculino());
+        System.out.println("quantidade de clientes mulheres: "+ mora.publicoFeminino());
     }
 }
